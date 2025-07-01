c511113290e5010e53ba462a84f0401f371d34d5
MoustafaAbdelrahimGaallouss <mga3llouss@gmail.com>
2025-07-01 18:12:47 +0300

    finish note button sheet (add custom button for adding note)

--- lib/views/widget/add_note_button_sheet.dart
+++ lib/views/widget/add_note_button_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/constants.dart';
+import 'package:note_app/views/widget/custom_button.dart';
 import 'package:note_app/views/widget/custom_text_field.dart';
 
 class addNoteModelsButtonSheet extends StatelessWidget {
@@ -9,19 +10,39 @@ class addNoteModelsButtonSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 16),
-      child: Column(
-        children: [
-          customTextField(
-            hintText: 'Title',
-          ),
-          SizedBox(
-            height: 24,
-          ),
-          customTextField(
-            hintText: 'content',
-            maxLines: 10,
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Add Note',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            customTextField(
+              hintText: 'Title',
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            customTextField(
+              hintText: 'content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            customButton(),
+            SizedBox(
+              height: 24,
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/views/widget/custom_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+
+
+class customButton extends StatelessWidget {
+  const customButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kPrimaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        onPressed: () {},
+        child: Text(
+          'Add',
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
