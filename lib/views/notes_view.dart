848ba642ff6b31e2a22e7d755780348dbc51c1ec
MoustafaAbdelrahimGaallouss <mga3llouss@gmail.com>
2025-07-10 23:17:46 +0300

    slove problem of kayboard scroll in addNoteModelsButtonSheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -14,6 +14,7 @@ class notesView extends StatelessWidget {
           backgroundColor: kPrimaryColor,
           onPressed: () {
             showModalBottomSheet(
+                isScrollControlled: true,
                 context: context,
                 builder: (context) {
                   return addNoteModelsButtonSheet();
--- lib/views/widget/add_note_button_sheet.dart
+++ lib/views/widget/add_note_button_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:note_app/cubits/add_note_cubite/add_note_cubit_cubit.dart';
 import 'package:note_app/views/widget/add_note_form.dart';
 
@@ -25,7 +24,11 @@ class addNoteModelsButtonSheet extends StatelessWidget {
           return AbsorbPointer(
             absorbing: state is AddNoteLoadingState ? true : false,
             child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 16),
+              padding: EdgeInsets.only(
+                left: 16,
+                right: 16,
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
               child: SingleChildScrollView(
                 child: const addNoteForm(),
               ),
